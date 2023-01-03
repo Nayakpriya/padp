1d0700fdb4cf6d465a7ed785fc21da5ab9bcc3fd
Priya Nayak <71636904+Nayakpriya@users.noreply.github.com>
2023-01-03 20:01:59 +0530

    Update msg.cpp

--- msg.cpp
+++ msg.cpp
@@ -9,6 +9,7 @@ int main(int argc,char *argv[])
 {
 int rank,root=0,tag=0,num,i;
 char msg[BUFFSIZE];
+  MPI_Init(&argc,&argv);
 MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 MPI_Comm_size(MPI_COMM_WORLD, &num);
 
